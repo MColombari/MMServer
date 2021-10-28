f5208b21346bbdd1b6f0dd0155cdabdf2a470b8b
mattiacolombari <mattia.colombari00@gmail.com>
2021-10-28 20:39:39 +0200

    Question and courses added.

--- src/ServerMM.java
+++ src/ServerMM.java
@@ -18,12 +18,44 @@ public class ServerMM{
 
         courseArrayList.add(new Course(1, "Analisi 1", "2021/2022", "Prof. Manfredini", "Corso su derivate, integrali e studio di funzioni."));
         courseArrayList.add(new Course(2, "Analisi 2", "2021/2022", "Prof. Villarini", "Corso su serie, potenziale e derivate parziali."));
+        courseArrayList.add(new Course(3, "Inglese", "2021/2022", "Prof. ___", "Corso B1 inglese."));
+        courseArrayList.add(new Course(4, "Geometria", "2021/2022", "Prof. Casali", "Corso di algebra lineare e geometria."));
+        courseArrayList.add(new Course(5, "Fondamenti di Informatica 1", "2021/2022", "Prof. ___", "Corso fondamenti di informatica"));
 
+        /* Analisi 1 */
         questionArrayList.add(new Question(1, 1, "20211025", "2 + 2", "4", "5", "10", "-e"));
         questionArrayList.add(new Question(2, 1, "20211025", "2 - 2", "0", "5", "10", "-e"));
         questionArrayList.add(new Question(3, 1, "20211025", "2 + 22", "24", "5", "10", "-e"));
         questionArrayList.add(new Question(4, 1, "20211025", "2 * 2", "4", "5", "10", "-e"));
         questionArrayList.add(new Question(5, 1, "20211025", "2 / 2", "1", "5", "10", "-e"));
+
+        /* Analisi 2 */
+        questionArrayList.add(new Question(6, 2, "20211025", "2 ^ 2", "4", "5", "10", "-e"));
+        questionArrayList.add(new Question(7, 2, "20211025", "10 * 10", "100", "5", "10", "-e"));
+        questionArrayList.add(new Question(8, 2, "20211025", "10 / 10", "1", "5", "10", "-e"));
+        questionArrayList.add(new Question(9, 2, "20211025", "2 ^ 10", "1024", "5", "10", "-e"));
+        questionArrayList.add(new Question(10, 2, "20211025", "2 ^ 8", "256", "5", "10", "-e"));
+
+        /* Inglese */
+        questionArrayList.add(new Question(11, 3, "20211025", "Translate: \"Dog\"", "Cane", "Gatto", "Mattia", "Lucertola"));
+        questionArrayList.add(new Question(12, 3, "20211025", "Translate: \"Hello\"", "Ciao", "Gatto", "Buona sera", "Addio"));
+        questionArrayList.add(new Question(13, 3, "20211025", "Translate: \"Object-oriented programming\"", "Programmazione orientata agli oggetti", "Analisi", "Matematica", "Geografia"));
+        questionArrayList.add(new Question(14, 3, "20211025", "Translate: \"Door\"", "Porta", "Finestra", "Veranda", "Balcone"));
+        questionArrayList.add(new Question(15, 3, "20211025", "Translate: \"Lizard\"", "Lucertola", "Gatto", "Cane", "Ciao"));
+
+        /* Geometria */
+        questionArrayList.add(new Question(16, 4, "20211025", "Quante facce ha un triangolo?", "3", "0", "1", "-1"));
+        questionArrayList.add(new Question(17, 4, "20211025", "Quante facce ha un rettangolo?", "4", "0", "1", "-1"));
+        questionArrayList.add(new Question(18, 4, "20211025", "Quanti angoli ha un triangolo?", "3", "0", "1", "-1"));
+        questionArrayList.add(new Question(19, 4, "20211025", "Quanti angoli ha un rettangolo?", "4", "0", "1", "-1"));
+        questionArrayList.add(new Question(20, 4, "20211025", "Quanti angoli ha un cerchio?", "0", "10", "1", "-1"));
+
+        /* Fondamenti di informatica 1 */
+        questionArrayList.add(new Question(21, 5, "20211025", "Converti in decimale la seguente stringa binaria: \"1001\"", "9", "0", "1", "7"));
+        questionArrayList.add(new Question(22, 5, "20211025", "Converti in decimale la seguente stringa binaria: \"1101\"", "13", "0", "1", "7"));
+        questionArrayList.add(new Question(23, 5, "20211025", "Converti in decimale la seguente stringa binaria: \"0001\"", "1", "0", "9", "7"));
+        questionArrayList.add(new Question(24, 5, "20211025", "Converti in decimale la seguente stringa binaria: \"0000\"", "0", "1", "9", "7"));
+        questionArrayList.add(new Question(25, 5, "20211025", "Converti in decimale la seguente stringa binaria: \"0111\"", "7", "1", "9", "0"));
     }
 
     public void startServer() {
